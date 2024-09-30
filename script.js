3543327263656bf755daeb6f67253de643c27837
Shaka <shakawillock@gmail.com>
2024-09-30 15:28:31 -0400

    Add functionality for user number input

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLengthEl = document.getElementById("password-length");
+let feedbackMessageEl = document.getElementById("feedback-message");
 let passwordOneEl = document.getElementById("generate-password-one");
 let passwordTwoEl = document.getElementById("generate-password-two");
-let buttonEl = document.getElementById('button-el');
+let buttonEl = document.getElementById("button-el");
 
 let randomPasswordOne = "";
 let randomPasswordTwo = "";
 
+let userInput;
+
 buttonEl.addEventListener('click', generateRandomPasswords);
 
 function generateRandomPasswords() {
+    if (passwordLengthEl.value === "" || passwordLengthEl.value < 8) {
+        feedbackMessageEl.textContent = "Please enter a password length between 8 and 128"
+    } else {
+        feedbackMessageEl.textContent = "";
+        userInput = passwordLengthEl.value;
+    }
+
     if (randomPasswordOne !== "" && randomPasswordTwo !== "") {
         randomPasswordOne = "";
         randomPasswordTwo = "";
     }
     
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < userInput; i++) {
         let randomIndexOne = Math.floor(Math.random() * characters.length);
         let randomIndexTwo = Math.floor(Math.random() * characters.length);
         randomPasswordOne += characters[randomIndexOne];
