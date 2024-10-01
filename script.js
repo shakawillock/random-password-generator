01de9ce3e3248ba51edc27198b497df4b97674ed
Shaka <shakawillock@gmail.com>
2024-09-30 22:37:15 -0400

    Add functions
    - Add function to check password length
    
    - Add function to check user selection
    
    - Add function to check password generator output
    
    - Add function to output random passwords

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
 const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-// "/"];
 let passwordLengthEl = document.getElementById("password-length");
 let lettersEl = document.getElementById("letters");
 let numbersEl = document.getElementById("numbers");
@@ -25,19 +25,26 @@ let userSelection = [];
 buttonEl.addEventListener('click', generateRandomPasswords);
 
 function generateRandomPasswords() {
+    checkPasswordLength();
+    checkUserSelection();
+    checkPasswordGeneratorOutput();
+    outputRandomPasswords();
+}
+
+function checkPasswordLength() {
     if (passwordLengthEl.value === "" || passwordLengthEl.value < 8) {
         feedbackMessageEl.textContent = "Please enter a password length between 8 and 128"
     } else {
         feedbackMessageEl.textContent = "";
         userInput = passwordLengthEl.value;
     }
+}
 
-    // Check if userSelection is empty
+function checkUserSelection() {
     if (userSelection.length !== 0) {
         userSelection = [];
     }
 
-    // Checks user selection
     if (lettersEl.checked === true) {
         for (let i = 0; i < letters.length;  i++) {
             userSelection.push(letters[i])
@@ -50,33 +57,37 @@ function generateRandomPasswords() {
         }
     }
 
-
     if (symbolsEl.checked === true) {
         for (let i = 0; i < symbols.length; i++) {
             userSelection.push(symbols[i]);
         }
     }
+}
 
-    console.log(userSelection);
-
+function checkPasswordGeneratorOutput() {
     if (randomPasswordOne !== "" && randomPasswordTwo !== "") {
         randomPasswordOne = "";
         randomPasswordTwo = "";
     }
-    
-    // for (let i = 0; i < userInput; i++) {
-    //     let randomIndexOne = Math.floor(Math.random() * characters.length);
-    //     let randomIndexTwo = Math.floor(Math.random() * characters.length);
-    //     randomPasswordOne += characters[randomIndexOne];
-    //     randomPasswordTwo += characters[randomIndexTwo];
-    // }
+}
 
-    for (let i = 0; i < userInput; i++) {
-        let randomIndexOne = Math.floor(Math.random() * userSelection.length);
-        let randomIndexTwo = Math.floor(Math.random() * userSelection.length);
-        randomPasswordOne += userSelection[randomIndexOne];
-        randomPasswordTwo += userSelection[randomIndexTwo];
+function outputRandomPasswords() {
+    if (userSelection.length === 0) {
+       for (let i = 0; i < userInput; i++) {
+            let randomIndexOne = Math.floor(Math.random() * characters.length);
+            let randomIndexTwo = Math.floor(Math.random() * characters.length);
+            randomPasswordOne += characters[randomIndexOne];
+            randomPasswordTwo += characters[randomIndexTwo];
+       }
+    } else {
+        for (let i = 0; i < userInput; i++) {
+            let randomIndexOne = Math.floor(Math.random() * userSelection.length);
+            let randomIndexTwo = Math.floor(Math.random() * userSelection.length);
+            randomPasswordOne += userSelection[randomIndexOne];
+            randomPasswordTwo += userSelection[randomIndexTwo];
+        }
     }
-        passwordOneEl.textContent = randomPasswordOne;
-        passwordTwoEl.textContent = randomPasswordTwo;
+
+    passwordOneEl.textContent = randomPasswordOne;
+    passwordTwoEl.textContent = randomPasswordTwo;
 }
